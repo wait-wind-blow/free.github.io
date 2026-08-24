2e318fe461d354f25c19b6bc209f8dc63c466351
liuyao <2740142339@qq.com>
2026-08-24 14:49:40 +0800

    release: remove mobile installer from GitHub Pages

--- chengda-ai/admin/activation/sw.js
+++
@@ -1,8 +0,0 @@
-const CACHE = "chengda-admin-v0.4.3";
-const ASSETS = ["./", "./styles.css", "./app.js", "./manifest.webmanifest", "/assets/chengda-logo.png"];
-self.addEventListener("install", (event) => event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(ASSETS))));
-self.addEventListener("activate", (event) => event.waitUntil(caches.keys().then((keys) => Promise.all(keys.filter((key) => key !== CACHE).map((key) => caches.delete(key))))));
-self.addEventListener("fetch", (event) => {
-  if (event.request.method !== "GET" || new URL(event.request.url).pathname.startsWith("/api/")) return;
-  event.respondWith(fetch(event.request).catch(() => caches.match(event.request)));
-});
